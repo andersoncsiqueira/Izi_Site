03c4f6d858309eff679549f870ad72735d00e4a7
Anderson Siqueira <92267959+andersoncsiqueira@users.noreply.github.com>
2022-05-25 15:55:59 -0300

    ok

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js"
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-firestore.js"
+  import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-firestore.js"
   
   const firebaseConfig = {
     apiKey: "AIzaSyDMNXtG5FmaswVHYIq4Rn6gT1TZN_bg_ho",
@@ -10,12 +10,18 @@
     messagingSenderId: "671329066253",
     appId: "1:671329066253:web:8e55ec3a21d626ccaf6444",
     measurementId: "G-BT6ZJW0RE4"
-  };
+  }
 
 
-  const app = initializeApp(firebaseConfig);
+  const app = initializeApp(firebaseConfig)
   const db = getFirestore(app)
-console.log(db)
+
+
+  getDocs(collection(db,"Produtos"))
+        .then(querySnapshot=>{
+            console.log(querySnapshot)
+        })
+        .catch(console.log)
 
 
 
