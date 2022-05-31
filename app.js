5d1630a6eb67d1f62cb590699876134dabf2449e
Anderson Siqueira <andersonboyjua@gmail.com>
2022-05-31 15:26:14 -0300

    edit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js"
-  import { getFirestore, collection, getDocs, addDoc } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-firestore.js"
+  import { getFirestore, collection, getDocs, addDoc, doc, deleteDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-firestore.js"
   
 
   const tbody = document.querySelector('[data-js="tbody"]')
@@ -19,21 +19,22 @@
   const collectionprodutos = collection(db,"Produtos")
 
   const formAddGame = document.querySelector('[data-js="form"]')
+  const buttonEdit = document.querySelector('[data-js="edit"]')
 
   getDocs(collectionprodutos)
         .then(querySnapshot=>{    
           let template = ""
     const templateForTable = querySnapshot.docs.map((doc) => {
       const {name,datebuy,custo,descricao, datearrive} = doc.data()
-             template += `<tr>
+             template += `<tr data-tr="${doc.id}">
                     <td>${name}</td>
                     <td>${descricao}</td>
                     <td>${datebuy}</td>
                     <td>${datearrive}</td>
-                    <td> R$ ${custo}</td>
+                    <td>${custo}</td>
                     <td>
-                    <i><img src="engrenagem.png" alt=""></i>
-                    <img src="lixeira.png" alt=""></i>
+                    <i><img src="engrenagem.png" data-edit="${doc.id}" alt=""></i>
+                    <i "><img  src="lixeira.png" data-delete="${doc.id}" alt=""></i>
                     </td>
                     </tr>
                     `
@@ -58,3 +59,54 @@ addDoc(collectionprodutos,{
 .catch(console.log)
 
 }) 
+
+tbody.addEventListener('click', e => {
+  const idDeleteClicked = e.target.dataset.delete
+  const idEditClicked = e.target.dataset.edit
+ 
+  if(idDeleteClicked){
+    
+    deleteDoc(doc(db, "Produtos",idDeleteClicked))
+    .then(doc => {
+      const trDeleted = document.querySelector(`[data-tr="${idDeleteClicked}"]`)
+      trDeleted.remove()
+      console.log('Removido')})
+    .catch(console.log)
+  }
+
+  if(idEditClicked) {
+    const bodyForm = document.querySelector('form')
+    bodyForm.style.background = 'orange'
+    const trEdited = document.querySelector(`[data-tr="${idEditClicked}"]`)
+    
+    bodyForm.children[1].value = trEdited.children[0].textContent
+    bodyForm.children[3].value = trEdited.children[1].textContent
+    bodyForm.children[6].value = trEdited.children[2].textContent
+    bodyForm.children[8].value = trEdited.children[3].textContent
+    bodyForm.children[11].value = trEdited.children[4].textContent
+    bodyForm.children[13].value = idEditClicked
+
+   
+  }
+
+})
+
+
+
+buttonEdit.addEventListener('click', e => {
+  e.preventDefault()
+  const bodyForm = document.querySelector('form')
+  console.log(bodyForm.children)
+
+  updateDoc(doc(db,'Produtos',bodyForm.children[13].value),{
+    name: bodyForm.children[1].value,
+    datebuy: bodyForm.children[6].value,
+    descricao: bodyForm.children[3].value,
+    datearrive: bodyForm.children[8].value,
+    custo: bodyForm.children[11].value
+  })
+  .then(doc => console.log(doc))
+  .catch(console.log)
+
+
+})
