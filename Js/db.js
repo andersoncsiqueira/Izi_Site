3fa8636a6d3a186298ba981c22046ab11c6bfd91
Anderson Siqueira <92267959+andersoncsiqueira@users.noreply.github.com>
2025-03-21 06:58:19 -0300

    save

--- Js/db.js
+++ Js/db.js
@@ -1,36 +1,62 @@
-
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
-  import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-firestore.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyCrxlosMCoYIOLQf56lFu4Ac-MyXOaGZwk",
-    authDomain: "estoque-izi.firebaseapp.com",
-    projectId: "estoque-izi",
-    storageBucket: "estoque-izi.firebasestorage.app",
-    messagingSenderId: "173339853396",
-    appId: "1:173339853396:web:6771bd1254584e78d323c1",
-    measurementId: "G-EYCLDNJ31Q"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-  const db = getFirestore(app)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-firestore.js";
+import { getFirestore, collection, getDocs, getDoc, doc } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 
-  getDocs(collection(db,'NOTEBOOKS'))
-  .then((querySnapshot)=>{
-    querySnapshot.forEach(element => {
-      console.log(element.data(),
-    element)
+const analytics = getAnalytics(app);
+const db = getFirestore(app)
+
+/*
+
+getDocs(collection(db,'NOTEBOOKS'))
+.then((querySnapshot)=>{
+  querySnapshot.forEach(element => {
+    console.log(element.data(),
+  element)
+    console.log(
+
+  )
+
+  console.log(element.data())
+    
+  });
+
+})
+.catch(console.log)
+.catch(console.log)
+
+*/
+
+const colecao = 'NOTEBOOKS'
+const id5490 = 'eRqlvo7jgZUK4mf3bsB9'
+const idCard = document.getElementById("dynamicLinkImg")
+
+async function getDocumento(colecao, idDocumento, img,a) {
+  try {
+    const docRef = doc(db, colecao, idDocumento);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
       
-    });
+      img.src = docSnap.data().path;
+      
+      a.innerText =  docSnap.data().MODELO
 
-  })
-  .catch(console.log)
+      
+      return docSnap.data().path;
+    } else {
+      console.log("No such document!");
+      return null;
+    }
+  } catch (error) {
+    console.error("Erro ao buscar documento:", error);
+    return null;
+  }
+}
+
+
+
+export {getDocumento}
