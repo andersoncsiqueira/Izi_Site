dbc91590f47f33952fbbedbb34755d9736a6d104
Anderson Siqueira <92267959+andersoncsiqueira@users.noreply.github.com>
2025-03-27 14:34:48 -0300

    product

--- Js/products.js
+++ Js/products.js
@@ -4,6 +4,8 @@ import { getDocumento, getDoc, db, doc } from "/Js/db.js";
 
 
 const allProducts = document.querySelectorAll(".products")
+const infos = document.querySelector(".infosP")
+let titulo = document.querySelector(".titulo")
 
 let ids = [
 "2SmCjPTsuofSSm0P5I0W",
@@ -32,6 +34,10 @@ a.href = `/pages/produto.html?id=${ids[index]}`
 card.appendChild(a)
 
 
+
+
+
+
  
 })
 
@@ -49,8 +55,73 @@ card.appendChild(a)
 
 const notebook = await getDoc(doc(db,'NOTEBOOKS',id))
 
-console.log(notebook.data())
+const h3 = document.createElement('h3')
 
- }
+const lista = document.createElement("ul")
+
+const span = document.createElement('span')
+const span1 = document.createElement('span')
+const span2 = document.createElement('span')
+const span3 = document.createElement('span')
+const span4 = document.createElement('span')
+const span5 = document.createElement('span')
+
+const valor = document.createElement('span')
+const processador = document.createElement('span')
+const memoria = document.createElement('span')
+const ssd  = document.createElement('span')
+const bateria = document.createElement('span')
+const condicao = document.createElement('span')
+
+
+h3.innerText = notebook.data().MODELO
+titulo.innerText = notebook.data().MODELO
+
+span.innerText = "Valor:"
+span1.innerText = "Processador:"
+span2.innerText = "Memória Ram:"
+span3.innerText = "SSD:"
+span4.innerText = "Saúde da batéria:"
+span5.innerText = "Condição:"
+
+const li = document.createElement("li")
+const li1= document.createElement('li')
+const li2 = document.createElement('li')
+const li3  = document.createElement('li')
+const li4 = document.createElement('li')
+const li5 = document.createElement('li')
+
+li.appendChild(span)
+li.appendChild(valor)
+
+
+li1.appendChild(span1)
+li1.appendChild(processador)
+
+
+li2.appendChild(span2)
+li2.appendChild(memoria)
+
+
+li3.appendChild(span3)
+li3.appendChild(ssd)
+
+li4.appendChild(span4)
+li4.appendChild(bateria)
+
+li5.appendChild(span5)
+li5.appendChild(bateria)
+
+lista.appendChild(li)
+lista.appendChild(li1)
+lista.appendChild(li2)
+lista.appendChild(li3)
+lista.appendChild(li4)
+lista.appendChild(li5)
+
+infos.append(h3)
+infos.append(lista)
+
+}
 
  postInfosInProducts(id)
