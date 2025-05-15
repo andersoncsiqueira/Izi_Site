316dfccdff2c3ff2d53b2b403e7de14917eb1fe4
Anderson Siqueira <92267959+andersoncsiqueira@users.noreply.github.com>
2025-05-15 10:36:22 -0300

    Filtro ok

--- Js/notebooks.js
+++ Js/notebooks.js
@@ -19,7 +19,7 @@ filtro.addEventListener("change", e => {
     
     const selecao = e.target.value
     let forRend = []
-    
+    let priceAll = []
 async function ordem () {
         
         const allDocs = await getDocs(collection(db, "NOTEBOOKS"))
@@ -249,6 +249,151 @@ async function ordem () {
                         console.log(renderizado.data())
 
                 
+
+
+                const card = document.createElement("div")
+                    const img = document.createElement("img")
+                    const ul = document.createElement("ul")
+                    const li = document.createElement("li")
+                    const a = document.createElement("a")
+                    const id = renderizado.id
+                    const spanValor = document.createElement("span")
+                    const spanMarca = document.createElement("span")
+
+                    li.innerText = `Modelo: ${renderizado.data().MODELO}`
+                    a.setAttribute("href",`/pages/produto.html?id=${id}`)
+
+                    img.setAttribute("src",`\\imagens\\modelos`+`\\${renderizado.data().MODELO}\\01.jpg`)
+                    spanValor.innerText = `Preço: ${formatarParaReais(renderizado.data().VENDA)}`
+                    spanMarca.innerText = `Marca: ${renderizado.data().MARCA}`
+
+                    card.classList.add("cardNote")
+                    containsAllNotebooks.appendChild(card)
+                    card.append(ul)
+                    ul.append(li)
+                    card.append(img)
+                    card.append(spanValor)
+                    card.append(spanMarca)
+                    card.append(a)
+                    a.innerText = "Clique aqui para mais detalhes"
+
+                    if(renderizado.data().ESTOQUE) {
+                        const span = document.createElement("span")
+                        span.innerHTML = `<i class="fas fa-circle" style="color: #2ecc71; font-size: 0.9em;"></i> Em estoque.`
+                        card.append(span)
+                    } else {
+                        const span = document.createElement("span")
+                        span.innerHTML = `<i class="fas fa-circle" style="color: #cca72e; font-size: 0.9em;"></i> Para Encomenda.`
+                        card.append(span)
+                    }
+
+
+            }
+        
+            break;
+
+
+         case "menorP":
+
+            containsAllNotebooks.innerHTML = ""
+            forRend = []
+        
+            for (const item of idsDocs) {
+                     const ref = doc(db, "NOTEBOOKS", `${item}`);
+                     
+                     const result = await getDoc(ref)
+
+                     priceAll.push({"key":item,"Price":result.data().VENDA})
+                     
+            }
+
+             priceAll = priceAll.sort((a,b) => Number(a.Price) - Number(b.Price))
+
+
+            for(const obj of priceAll){
+
+                forRend.push(obj.key)
+
+            }
+           
+        
+            for(const notebok of forRend ) {
+
+                        const ref = doc(db, "NOTEBOOKS", `${notebok}`)
+                        const renderizado = await getDoc(ref)
+                        console.log(renderizado.data())
+
+
+                const card = document.createElement("div")
+                    const img = document.createElement("img")
+                    const ul = document.createElement("ul")
+                    const li = document.createElement("li")
+                    const a = document.createElement("a")
+                    const id = renderizado.id
+                    const spanValor = document.createElement("span")
+                    const spanMarca = document.createElement("span")
+
+                    li.innerText = `Modelo: ${renderizado.data().MODELO}`
+                    a.setAttribute("href",`/pages/produto.html?id=${id}`)
+
+                    img.setAttribute("src",`\\imagens\\modelos`+`\\${renderizado.data().MODELO}\\01.jpg`)
+                    spanValor.innerText = `Preço: ${formatarParaReais(renderizado.data().VENDA)}`
+                    spanMarca.innerText = `Marca: ${renderizado.data().MARCA}`
+
+                    card.classList.add("cardNote")
+                    containsAllNotebooks.appendChild(card)
+                    card.append(ul)
+                    ul.append(li)
+                    card.append(img)
+                    card.append(spanValor)
+                    card.append(spanMarca)
+                    card.append(a)
+                    a.innerText = "Clique aqui para mais detalhes"
+
+                    if(renderizado.data().ESTOQUE) {
+                        const span = document.createElement("span")
+                        span.innerHTML = `<i class="fas fa-circle" style="color: #2ecc71; font-size: 0.9em;"></i> Em estoque.`
+                        card.append(span)
+                    } else {
+                        const span = document.createElement("span")
+                        span.innerHTML = `<i class="fas fa-circle" style="color: #cca72e; font-size: 0.9em;"></i> Para Encomenda.`
+                        card.append(span)
+                    }
+
+
+            }
+        
+            break;
+
+             case "maiorP":
+
+            containsAllNotebooks.innerHTML = ""
+            forRend = []
+        
+            for (const item of idsDocs) {
+                     const ref = doc(db, "NOTEBOOKS", `${item}`);
+                     
+                     const result = await getDoc(ref)
+
+                     priceAll.push({"key":item,"Price":result.data().VENDA})
+                     
+            }
+
+             priceAll = priceAll.sort((a,b) => Number(b.Price) - Number(a.Price))
+
+
+            for(const obj of priceAll){
+
+                forRend.push(obj.key)
+
+            }
+           
+        
+            for(const notebok of forRend ) {
+
+                        const ref = doc(db, "NOTEBOOKS", `${notebok}`)
+                        const renderizado = await getDoc(ref)
+                        console.log(renderizado.data())
 
 
                 const card = document.createElement("div")
